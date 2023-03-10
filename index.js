c4b3b48ed82bfd564d374f99df04e71f31a7c3e7
Martin <martinbirgell@gmail.com>
2023-03-10 21:40:59 +0100

    b4 change to select instead of bulma dropdown

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const starWarsApi = "https://swapi.dev/api"
 let allPeopleData = []
+let leftPerson
+let rightPerson
 
 class Character {
     constructor(name, gender, height, mass, hairColor, skinColor, eyeColor, movies, pictureUrl) {
@@ -15,6 +17,7 @@ class Character {
     }
 }
 
+
 // Takes the string and fetches from that adress
 const apiGet = async str => {
     const res = await fetch(str)
@@ -24,23 +27,38 @@ const apiGet = async str => {
 
 // Fetches all people and continuesly updates the lists
 const getAllPeople = async () => {
+    let data
     for (let i = 0; i < 20; i++) {
-        if (i === 0 || allPeopleData.at(-1).next !== null) {
-            const data = await apiGet(starWarsApi + (i === 0 ? `/people` : `/people?page=${i + 1}`))
-            allPeopleData.push(await data)
-            renderLists(data)
+        if (i === 0 || data.next !== null) {
+            data = await apiGet(starWarsApi + (i === 0 ? `/people` : `/people?page=${i + 1}`))
+            data.results.forEach(person => allPeopleData.push({...person, selected: false}))
+            allPeopleData.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+            renderDropdowns()
         } else {
-            console.log(allPeopleData)
-            break // Exits the for loop
+            break // Exits the for loop when there are no more data to fetch
         }
     }
+    console.log(allPeopleData)
 }
 
-const renderLists = (obj) => {
-    let dropdown = document.querySelectorAll(".dropdown-content")    
-    obj.results.forEach(person => {
-        dropdown.forEach(dropdown => {
-            dropdown.innerHTML += `<div class="dropdown-item">${person.name}</div>`
+const renderDropdowns = () => {
+    let dropdowns = document.querySelectorAll(".dropdown-content")
+    dropdowns.forEach(elem => elem.innerHTML = "")
+    allPeopleData.forEach((person, index) => {
+        dropdowns.forEach(dropdown => {
+            let listItem = document.createElement("div")
+            listItem.classList.add("dropdown-item", "is-clickable", person.selected&& "selected")
+            listItem.innerText = person.name
+            dropdown.append(listItem)
+            
+            listItem.addEventListener("mousedown", (() => {
+                const left = dropdown.classList.contains("left")
+                const header = document.querySelector(`#${left ? "left" : "right"}-container h2`)
+                header.innerText = person.name
+                left ? leftPerson = person : rightPerson = person
+                allPeopleData[index].selected = true
+                renderDropdowns()
+            }))
         })
     })    
 }
@@ -48,14 +66,12 @@ const renderLists = (obj) => {
 
 //? Event listeners --------------------------------------------------------------------
 
+// Dropdown show/hide
 document.querySelectorAll(".dropdown-trigger").forEach(async (button, index) => {
-    button.addEventListener("click", e => {
-    let dropdown = document.querySelector(`#dropdown-menu${index + 1}`)
-        dropdown.classList.toggle("is-hidden")
-    })
+    button.addEventListener("click", () => { document.querySelector(`#dropdown-menu-${index === 0 ? "left" : "right"}`).classList.toggle("is-hidden")})
+    button.addEventListener("focusout", () => { document.querySelector(`#dropdown-menu-${index === 0 ? "left" : "right"}`).classList.add("is-hidden")})
 })
 
 //? Running code -----------------------------------------------------------------------
 
 getAllPeople() // Fetches all people and fills the dropdown
-
